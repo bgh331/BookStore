ad34c7b1b06ad417a761e774bc91d2942a3b909e
Kiia Kaukonen <bgh331@myy.haaga-helia.fi>
2020-02-06 15:46:15 +0200

    First Commit

---
+++ src/main/java/com/example/BookStore/web/BookStoreController.java
@@ -0,0 +1,18 @@
+package com.example.BookStore.web;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class BookStoreController {
+
+@RequestMapping(value= "/index", method = RequestMethod.GET)
+
+
+	
+
+}
